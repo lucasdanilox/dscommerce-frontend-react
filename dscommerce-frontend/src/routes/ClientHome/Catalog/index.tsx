80113f89e6a758f640d773da2aa4e3c946ac3c25
Lucas Danilo <lukastecno@gmail.com>
2024-03-11 16:53:48 -0300

    Dynamically product catalog

--- dscommerce-frontend/src/routes/ClientHome/Catalog/index.tsx
+++ dscommerce-frontend/src/routes/ClientHome/Catalog/index.tsx
@@ -1,32 +1,9 @@
 import ButtonNextPage from '../../../components/ButtonNextPage';
 import CatalogCard from '../../../components/CatalogCard';
 import SearchBar from '../../../components/SearchBar';
-import { ProductDTO } from '../../../models/product';
+import * as productService from '../../../services/product-service';
 import './styles.css'
 
-const product: ProductDTO = {
-
-    id: 2,
-    name: "Smart TV",
-    description: "TV 50 Polegadas",
-    imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg",
-    price: 2500.99,
-    categories: [
-        {
-            id: 2,
-            name: "Eletrônicos"
-        },
-        {
-            id: 3,
-            name: "Computadores"
-        },
-        {
-            id: 4,
-            name: "SmarTV"
-        }
-    ]
-
-}
 
 export default function Catalog() {
 
@@ -35,18 +12,11 @@ export default function Catalog() {
             <section id="catalog-section" className="dsc-container">
                 <SearchBar />
                 <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
+                    {
+                        productService.findAll()
+                            .map(product => <CatalogCard key={product.id} product={product} />)
+                    }
                 </div>
-
                 <ButtonNextPage />
             </section>
         </main>
