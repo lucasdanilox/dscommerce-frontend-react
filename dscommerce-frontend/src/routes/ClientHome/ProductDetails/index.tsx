f434ea989e9879f5a9cacdd8c1f7af2831a8f6da
Lucas Danilo <lukastecno@gmail.com>
2024-03-19 17:05:17 -0300

    Fix: Modules

--- dscommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
+++ dscommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
@@ -5,12 +5,20 @@ import ButtonInverse from '../../../components/ButtonInverse';
 import * as ProductService from '../../../services/product-service';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { ProductDTO } from '../../../models/product';
 
 export default function ProductDetails() {
 
     const params = useParams();
 
-    const product = ProductService.findById(Number(params.productId));
+
+    const [product, setProduct] = useState<ProductDTO>();
+
+    useEffect(() => {
+        const prod = ProductService.findById(Number(params.productId));
+        setProduct(prod);
+    }, []);
 
     return (
         <main>
@@ -23,9 +31,9 @@ export default function ProductDetails() {
                 <div className="dsc-btn-page-container">
                     <ButtonPrimary text="Comprar" />
                     <Link to="/">
-                    <ButtonInverse text="Início" />
+                        <ButtonInverse text="Início" />
                     </Link>
-                    
+
                 </div>
             </section>
         </main>
